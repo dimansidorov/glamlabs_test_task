051d660979a4c0ed44226155164aa9b3f4bb29fe
dimansidorov <dimkasidorow@gmail.com>
2023-08-18 17:46:42 +0200

    Update dependencies.py

--- routers/instagram.py
+++ routers/instagram.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter
+import asyncio
+
+from fastapi import APIRouter, HTTPException
+
+
+# from dependencies import get_instagram_photos
 
 router = APIRouter(prefix='', tags=['Instagram'])
 
 
 @router.get("/getPhotos")
 async def get_photos(username: str, max_count: int):
-    return {
-        "urls": [username, max_count]
-    }
+    return {"urls": [username, max_count]}
+
+
