dbe5a4a45a1122234ba52b90349c407957389949
dimansidorov <dimkasidorow@gmail.com>
2023-08-21 13:10:31 +0200

    update dockerfile, docker-compose, instagram, dependecies.py

--- routers/instagram.py
+++ routers/instagram.py
@@ -1,13 +1,23 @@
 from fastapi import APIRouter
-
-from dependencies import get_instagram_photo_links
+from fastapi.responses import JSONResponse
+from dependencies import get_instagram_photo_links, EmptyPageException
 
 router = APIRouter(prefix='', tags=['Instagram'])
 
 
 @router.get("/getPhotos")
 async def get_photos(username: str, max_count: int):
-    result = await get_instagram_photo_links(username, max_count)
-    return {"urls": result}
+    try:
+        result = await get_instagram_photo_links(username, max_count)
+    except EmptyPageException:
+        return JSONResponse(
+            content={'error': 'This profile is empty or does not exist'},
+            status_code=400
+        )
+    else:
+        return JSONResponse(
+            content={"urls": result},
+            status_code=200
+        )
 
 
